23166603055e763b05c13f0cf62e8f597551172b
theAlphaGuardian <44818405+JosephLahiru@users.noreply.github.com>
2023-08-31 15:12:58 +0530

    Initial commit

---
+++ app/src/main/java/me/mtron/mobile_application_development_labsheet2/Activity2.java
@@ -0,0 +1,32 @@
+package me.mtron.mobile_application_development_labsheet2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class Activity2 extends AppCompatActivity {
+
+    TextView name;
+    TextView city;
+    Intent intent;
+    String data_name, data_city;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_2);
+
+        name = findViewById(R.id.textName);
+        city = findViewById(R.id.textCity);
+
+        intent = getIntent();
+
+        data_name = intent.getStringExtra("NAME");
+        data_city = intent.getStringExtra("CITY");
+
+        name.setText(data_name);
+        city.setText(data_city);
+    }
+}
---
+++ app/src/main/java/me/mtron/mobile_application_development_labsheet2/MainActivity.java
@@ -0,0 +1,33 @@
+package me.mtron.mobile_application_development_labsheet2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Intent intent;
+    Button go;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        intent = new Intent(this, Activity2.class);
+        go = findViewById(R.id.btnGo);
+
+        go.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                intent.putExtra("NAME", "Joe");
+                intent.putExtra("CITY", "Colombo");
+
+                startActivity(intent);
+            }
+        });
+    }
+}
